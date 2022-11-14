c14a5151cc3abf92012d01bc7bfc126e7ce96211
Vivien51380 <mereauxvivien@gmail.com>
2022-11-14 11:47:02 +0100

    add deleteUser

--- movieHandlers.js
+++ movieHandlers.js
@@ -69,6 +69,23 @@ const updateMovie = (req, res) => {
     });
 };
 
+const deleteMovie = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from movies where id = ?", [id])
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error deleting the movie");
+    });
+};
 
 
 
@@ -77,5 +94,6 @@ module.exports = {
   getMovieById,
   postMovie,
   updateMovie,
+  deleteMovie,
  
 };
